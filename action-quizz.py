835491759b776e6bb05e2fa4a4bbef090573e113
Robin Guignard-Perret <yxdunc@users.noreply.github.com>
2018-04-16 10:43:13 +0200

    init

---
+++ action-quizz.py
@@ -0,0 +1,65 @@
+from hermes_python.hermes import Hermes
+import times_tables as tt
+
+
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+INTENT_FILTER_GET_ANSWER = [
+    "give_answer",
+    "interrupt",
+    "does_not_know"
+]
+
+SessionsStates = {}
+
+def user_request_quizz(hermes, intentMessage):
+    print("User is asking for a quizz")
+
+    state = tt.start_quizz(intentMessage.slots.get("number"), intentMessage.slots.get("tables"))
+
+    tt.save_session_state(SessionsStates, intentMessage.session_id, state["session_state"])
+
+    hermes.publish_continue_session(intentMessage.session_id, state["response"], INTENT_FILTER_GET_ANSWER)
+
+
+def user_gives_answer(hermes, intentMessage):
+    print("User is giving an answer")
+
+    state = tt.check_user_answer(SessionsStates.get(intentMessage.session_id), intentMessage.slots.get("answer"))
+
+    if not state["continues"]:
+        hermes.publish_end_session(intentMessage.session_id, state["sentence"])
+        tt.remove_session_state(SessionsStates, intentMessage.session_id)
+        return
+
+    hermes.publish_continue_session(intentMessage.session_id, state["sentence"], INTENT_FILTER_GET_ANSWER)
+
+
+def user_does_not_know(hermes, intentMessage):
+    print("User does not know the answer")
+
+    state = tt.user_does_not_know(SessionsStates[intentMessage.session_id], intentMessage.slots["answer"])
+
+    if not state["continues"]:
+        hermes.publish_end_session(intentMessage.session_id, state["sentence"])
+        tt.remove_session_state(SessionsStates, intentMessage.session_id)
+        return
+
+    hermes.publish_continue_session(intentMessage.session_id, state["sentence"], INTENT_FILTER_GET_ANSWER)
+
+
+def user_quits(hermes, intentMessage):
+    print("User wants to quit")
+    session_id = intentMessage.session_id
+
+    hermes.publish_end_session(session_id, tt.terminate_early(SessionsStates, session_id))
+
+
+with Hermes(MQTT_ADDR) as h:
+    h.subscribe_intent("start_lesson", user_request_quizz) \
+        .subscribe_intent("interrupt", user_quits) \
+        .subscribe_intent("does_not_know", user_does_not_know) \
+        .subscribe_intent("give_answer", user_gives_answer) \
+        .start()
---
+++ times_tables.py
@@ -0,0 +1,105 @@
+import random
+
+
+def start_quizz(nb_questions=5, tables=[]):
+    x = random.randint(0, 12)
+    y = random.randint(0, 12)
+
+    if tables:
+        tables = map(int, tables)
+        y = tables[random.randint(0, len(tables))]
+
+    session_state = {
+        "x": x,
+        "y": y,
+        "good": 0,
+        "bad": 0,
+        "step": 0,
+        "tables": tables,
+        "nb_questions": nb_questions
+    }
+
+    return dict(response=_create_question(x, y, ""), session_state=session_state)
+
+
+def check_user_answer(session_state, answer):
+    correction_sentence, is_correct = _generate_correction(session_state.get("x"), session_state.get("y"), answer)
+
+    if is_correct: session_state["good"] += 1
+    if not is_correct: session_state["bad"] += 1
+
+    return _create_next_step(correction_sentence, session_state)
+
+
+def user_does_not_know(session_id, sessions_states):
+    if session_id is None or sessions_states.get(session_id) is None:
+        print "[WARNING] No current game on this session id"
+        return
+    session_state = sessions_states.get(session_id)
+    answer = session_state["x"] * session_state["y"]
+
+    return _create_next_step("That's ok don't worry, the answer was " + str(answer), session_state)
+
+
+def terminate_early(sessions_states, session_id):
+    remove_session_state(sessions_states, session_id)
+    return "As you wish, the game is over"
+
+
+def _create_next_step(sentence, session_state):
+    if session_state.get("step") < session_state.get("nb_questions"):
+        session_state["x"] = random.randint(0, 11)
+        session_state["y"] = random.randint(0, 11)
+
+        if session_state.get("tables") is not None:
+            tables = session_state["tables"]
+            session_state["y"] = tables[random.randint(0, len(tables))]
+
+        sentence = _create_question(session_state["x"], session_state["y"], sentence)
+        session_state["step"] += 1
+        continues = True
+    else:
+        sentence += " You had " + str(session_state["good"]) + " good answers and " + str(
+            session_state["bad"]) + " bad answers "
+        sentence = _create_end_sentence(sentence)
+        session_state = {}
+        continues = False
+
+    return dict(sentence=sentence, continues=continues, session_state=session_state)
+
+
+def save_session_state(sessions_states, session_id, new_state):
+    sessions_states[session_id] = _set_not_none_dict_value(sessions_states.get(session_id), new_state)
+
+
+def remove_session_state(sessions_states, session_id):
+    sessions_states[session_id] = None
+
+
+def _generate_correction(x, y, user_answer):
+    result = x * y
+    sentence = "That's it. Well done."
+    is_correct = True
+
+    if result != user_answer:
+        sentence = "Oh no, wrong answer. {} times {} is equal to {}".format(x, y, result)
+        is_correct = False
+
+    return sentence, is_correct
+
+
+def _set_not_none_dict_value(to_update, update):
+    to_update = to_update or {}
+    for key, value in update.iteritems():
+        if value is not None:
+            to_update[key] = value
+    return to_update
+
+
+def _create_end_sentence(sentence):
+    return sentence + " The game is over"
+
+
+def _create_question(x, y, intro_sentence=""):
+    question = "What is {} times {} ?".format(x, y)
+    return intro_sentence + " " + question
