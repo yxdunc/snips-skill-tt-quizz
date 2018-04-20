ee06c1d3a98a2b068e580c934afea7cd4a87a2a3
Robin Guignard-Perret <yxdunc@users.noreply.github.com>
2018-04-20 18:25:47 +0200

    debug

--- action-quizz.py
+++ action-quizz.py
@@ -26,9 +26,9 @@ def user_request_quiz(hermes, intent_message):
     number_of_questions = 1
     tables = []
 
-    if intent_message.slots.number.first():
+    if intent_message.slots.number:
         number_of_questions = intent_message.slots.number.first().value.value
-    if intent_message.slots.table.first():
+    if intent_message.slots.table:
         tables = [intent_message.slots.table.first().value.value]
 
     session_state, sentence = tt.start_quiz(number_of_questions, tables)
@@ -45,7 +45,7 @@ def user_gives_answer(hermes, intent_message):
     session_id = intent_message.session_id
     session_state = SessionsStates.get(session_id)
 
-    if intent_message.slots.answer.first():
+    if intent_message.slots.answer:
         answer = intent_message.slots.answer.first().value.value
 
     session_state, sentence, continues = tt.check_user_answer(session_state, answer)
